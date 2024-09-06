cefbcdbc554680b8c42ab02fe425a32c7661c9b9
=Hilmy1789 <=helmyzakim@gmail.com>
2024-09-06 09:16:34 +0700

    Praktikum 1 Pertemuan 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Praktikum 1 PWL
+
+Route::get('/hello', function () { 
+    return 'Hello World'; 
+    }); 
+
+Route::get('/world', function () { 
+    return 'World'; 
+    });
+
+Route::get('/', function () { 
+    return 'Selamat Datang'; 
+    });
+
+Route::get('/about', function () { 
+    return '2241760089|Hilmy Zaky Mustakim'; 
+    });
+
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+    }); 
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name; 
+});
